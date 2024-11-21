db20bd64d7141b2dc0151eb1bfe2927774e0c85c
NiklasWeber27 <niklasweber910@gmail.com>
2024-11-21 09:55:34 +0100

    Pokertests Änderungen

--- PokerTests.py
+++ PokerTests.py
@@ -11,8 +11,41 @@ class PokerTest(unittest.TestCase):
         self.assertEqual(Pokersimulator.getnumber(51, 13), 12)
 
     def test_ispair(self):
-        self.assertTrue(Pokersimulator.ispair([0, 13]))
+        self.assertTrue(Pokersimulator.ispair([0, 0]))
         self.assertFalse(Pokersimulator.ispair([0, 1, 2]))
+
+    def test_istwopairs(self):
+        self.assertTrue(Pokersimulator.istwopairs([0, 13, 0, 13]))
+        self.assertFalse(Pokersimulator.istwopairs([0, 1, 2, 3]))
+
+    def test_isdrilling(self):
+        self.assertTrue(Pokersimulator.isdrilling([0, 0, 0]))
+        self.assertFalse(Pokersimulator.isdrilling([0, 1, 2]))
+
+    def test_isstraight(self):
+        self.assertTrue(Pokersimulator.isstraight([0, 1, 2, 3, 4]))
+        self.assertFalse(Pokersimulator.isstraight([0, 1, 2, 3, 5]))
+
+    def test_isflush(self):
+        self.assertTrue(Pokersimulator.isflush([0, 0, 0, 0, 0]))
+        self.assertFalse(Pokersimulator.isflush([0, 0, 0, 0, 1]))
+
+    def test_isfullhouse(self):
+        self.assertTrue(Pokersimulator.isfullhouse([0, 0, 0, 1, 1]))
+        self.assertFalse(Pokersimulator.isfullhouse([0, 1, 2, 3, 4]))
+
+    def test_isvierling(self):
+        self.assertTrue(Pokersimulator.isvierling([0, 0, 0, 0]))
+        self.assertFalse(Pokersimulator.isvierling([0, 1, 2, 3, 4]))
+
+    def test_isstraightflush(self):
+        self.assertTrue(Pokersimulator.isstraightflush([0, 1, 2, 3, 4], [0, 0, 0, 0, 0]))
+        self.assertFalse(Pokersimulator.isstraightflush([0, 1, 2, 3, 5], [0, 0, 0, 0, 0]))
+
+    def test_isroyalflush(self):
+        self.assertTrue(Pokersimulator.isroyalflush([8, 9, 10, 11, 12], [0, 0, 0, 0, 0]))
+        self.assertFalse(Pokersimulator.isroyalflush([0, 1, 2, 3, 4], [0, 0, 0, 0, 0]))
+
 
 if __name__ == '__main__':
     unittest.main()
